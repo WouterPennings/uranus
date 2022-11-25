08856cde94aba5264e2e126c2f0ddc9fa6682632
WouterPennings <wouterpennings@gmail.com>
2022-11-25 22:38:52 +0100

    improved cookies by adding a abstraction

--- examples/cookies.ts
+++ examples/cookies.ts
@@ -1,14 +1,11 @@
-import { UranusHTTP, Cookie } from "../mod.ts";
+import { UranusHTTP, Cookies, Cookie } from "../mod.ts";
 
 const port = 3000;
 const app = new UranusHTTP(port);
 
 app.get("/cookies", async (req, res) => {
-    const cookie: Cookie = {
-        name: "ID",
-        value: "123",
-    }
-    res.cookies.set("ID", cookie);
+    const cookie: Cookie = new Cookie("ID", "123");
+    res.cookies.add(cookie);
     res.text("There now is a cookie with the name: 'ID'");
 });
 
---
+++ examples/http.ts
@@ -0,0 +1,57 @@
+import { Cookie, UranusHTTP } from "../lib/uranusHTTP.ts";
+import { requestLogger } from "../lib/middelwares.ts";
+import { testMiddleware, testMiddleware2 } from "./middlewares.ts";
+
+// Port 3000 is the port that this webserver will be hosted on
+const port = 3000;
+// Creating a UranusHTTP instance
+const app = new UranusHTTP(port);
+
+// Using the middlewar that stores all the headers of all the request is a .log file
+app.useMiddleware(requestLogger);
+
+// Return text
+app.get("/", async (req, res) => {
+    res.text("<h1>Hello, World!</h1>");
+});
+
+// Can also return the content of a file
+app.get("/file", testMiddleware, testMiddleware2, async (req, res) => {
+    res.sendFile("index.html");
+});
+
+// Returning JSON with a POST request. Also getting parameter from URL
+app.post("/json/{value}", async (req, res) => {
+    res.json({ "value": Number(req.parameters.value) });
+});
+
+// Return de body of the POST request
+app.post("/body", async (req, res) => {
+    res.json(req.bodyToJSON());
+});
+
+// Using a DELETE request
+app.delete("/person/{id}", async (req, res) => {
+    if (req.parameters.id == "123") {
+        res.text("123");
+    }
+});
+
+// You can also redirect to a different website
+app.get("/redirect", async (req, res) => {
+    res.redirect("https://google.com");
+});
+
+app.get("/cookies", async (req, res) => {
+    const cookie: Cookie = {
+        name: "ID",
+        value: "123",
+    }
+    res.cookies.set("ID", cookie);
+    res.text("There now is a cookie with the name: 'ID'");
+});
+
+// Starting the webserver with all the endpoints defined above
+app.start(() => {
+    console.log(`Listening on http://localhost:${port}`);
+});
