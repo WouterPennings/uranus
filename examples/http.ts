de0132b1c38138393b8c62be6c37cdd18d6a5807
WouterPennings <wouterpennings@gmail.com>
2022-11-25 01:38:21 +0100

    updates readme

--- examples/http.ts
+++ examples/http.ts
@@ -1,41 +1,44 @@
 import { UranusHTTP } from "../lib/uranusHTTP.ts";
 import { requestLogger } from "../lib/middelwares.ts";
 
+// Port 3000 is the port that this webserver will be hosted on
 const port = 3000;
+// Creating a UranusHTTP instance
 const app = new UranusHTTP(port);
 
+// Using the middlewar that stores all the headers of all the request is a .log file
 app.useMiddleware(requestLogger);
 
+// Return text
 app.get("/", async (req, res) => {
   res.text("Yea regular text, I know...")
 });
 
+// Can also return the content of a file
 app.get("/file", async (req, res) => {
   res.sendFile("index.html");
 });
 
+// Returning JSON with a POST request. Also getting parameter from URL
 app.post("/json/{value}", async (req, res) => {
-  res.json({"value": Number(req.parameters.value)});
+  res.json( { "value": Number(req.parameters.value) } );
 });
 
+// Return de body of the POST request
 app.post("/body", async (req, res) => {
-  res.text(`This was your body: ${req.body}\nI know kinda crazy`);
-});
-
-app.post("/bodytojson", async (req, res) => {
   res.json(req.bodyToJSON());
 });
 
+// Using a DELETE request
+app.delete('/person/{id}', async (req, res) => {
+  res.text(`Deleted person with id: ${req.parameters.id}`)
+});
+
+// You can also redirect to a different website
 app.get('/redirect', async (req, res) => {
   res.redirect("https://google.com");
 });
 
-app.get('/end', async (req, res) => {
-  res.end(404);
-});
 
-app.get('/person/{name}/blabla', async (req, res) => {
-  res.text(req.parameters.name);
-});
-
+// Starting the webserver with all the endpoints defined above
 app.start(() => { console.log(`Listening on http://localhost:${port}`) });
